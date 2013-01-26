cde981ed729480924d6363a6d353c00953915ccd
Mike Amundsen <mamund@yahoo.com>
2013-01-26 12:34:00 -0500

    code up class-schedule+xml representation module

--- server/representation.js
+++ server/representation.js
@@ -5,11 +5,9 @@
  * ****************************************/
 
 module.exports = main;
+
 var csType = 'application/vnd.apiacademy-schedule+xml';
 var xmlType = 'application/xml';
-
-var csDoc = {};
-csDoc.link = '<link name="{@name}" href="{@href}" action="{@action}" prompt="{@prompt}" />';
 
 function main(object, mimeType) {
     
@@ -26,23 +24,173 @@ function main(object, mimeType) {
 }
 
 function processCSDoc(object) {
-    var doc, i, x, tmp;
+    var doc, i, x, tmp, coll;
 
     doc = '<root>';
+
+    // handle link collection
     if(object && object.links) {
         doc += '<links>';
-        for(i=0, x=object.links.length; i<x; i++) {
-            tmp = csDoc.link;
-            tmp = tmp.replace('{@name}', object.links[i].name);
-            tmp = tmp.replace('{@href}', object.links[i].href);
-            tmp = tmp.replace('{@action}', object.links[i].action);
-            tmp = tmp.replace('{@prompt}', object.links[i].prompt);
-            doc += tmp;
+        coll = object.links;
+        for(i=0, x=coll.length; i<x; i++) {
+            doc += linkElement(coll[i]);
         }
         doc += '</links>';
+    }
+
+    // handle lists
+    if(object && object.lists) {
+        for(i=0,x=object.lists.length; i<x;i++) {
+            doc += listElement(object.lists[i]);
+        }
+    }
+
+    // handle error
+    if(object && object.error) {
+        doc += '<error>';
+        coll = object.error.data;
+        for(i=0, x=coll.length; i<x; i++) {
+            doc += dataElement(coll[i]);
+        }
+        doc += '</error>';
     }
     doc += '</root>';
 
     return doc;
+}
+
+function listElement(elm) {
+    var i, x, rtn;
+
+    rtn ='<list ';
+    if(elm.name) {
+        rtn += 'name="'+elm.name+'" ';
+    }
+    rtn += '>';
+
+    if(elm.item) {
+        for(i=0, elm.item.length; i<x; i++) {
+            rtn += itemElement(elm.item[i]);
+        }
+    }
+    rtn += '</list>';
+
+    return rtn;
+}
+
+function itemElement(elm) {
+    var coll, i, x, rtn;
+
+    rtn = '<item ';
+    if(elm.name) {
+        rnt += 'name="'+elm.name+'" ';
+    }
+    rtn += '>';
+
+    if(item.display) {
+        rtn += displayElement(item.display);
+    }
+
+    if(item.actions) {
+        rtn += actionElement(item.action);
+    }
+    rtn += '</item>';
+
+    return rtn;
+}
+
+function displayElement(elm) {
+    var i, x, rtn;
+
+    rtn = '<display>';
+    if(elm.data) {
+        for(i=0,x=elm.data.length; i<x; i++) {
+            rtn += dataElement(elm.data[i]);
+        }
+    }
+    rtn += '</display>';
+
+    return rtn;
+}
+
+function actionElement(elm) {
+    var i, x, rtn;
+
+    rtn = '<actions>';
+
+    if(elm.templates) {
+        for(i=0, x=elm.templates.length; i<x; i++) {
+            rtn += templateElement(elm.template[i]);
+        }
+    }
+    
+    if(elm.links) {
+        for(i=0, x=elm.links.length; i<x; i++) {
+            rtn += linkElement(elm.links[i]);
+        }
+    }
+
+    rtn += '</actions>';
+
+    return rtn;
+}
+
+function templateElement(elm) {
+    var i, x, rtn;
+
+    rtn = '<template ';
+    if(elm.name) {
+        rtn += 'name="'+elm.name+'" ';
+    }
+    if(elm.data) {
+        for(i=0, x=elm.data.length; i<x; i++) {
+            rtn += dataElement(elm.data[i]);
+        }
+    }
+    rtn += '</template>';
+
+    return rtn;
+}
+
+function linkElement(elm) {
+    var rtn;
+
+    rtn = '<link ';
+    if(elm.name) {
+        rtn += 'name="'+elm.name+'" ';
+    }
+    if(elm.href) {
+        rtn += 'href="'+elm.href+'" ';
+    }
+    if(elm.action) {
+        rtn += 'action="'+elm.action+'" ';
+    }
+    if(elm.prompt) {
+        rtn += 'prompt="'+elm.prompt+'" ';
+    }
+    rtn += '/>';
+
+    return rtn;
+}
+
+function dataElement(elm) {
+    var rtn;
+    
+    rtn = '<data ';
+    if(elm.name) {
+        rtn += 'name="'+elm.name+'" ';
+    }
+    if(elmprompt) {
+        rtn += 'prompt="'+elm.prompt+'" ';
+    }
+    if(elm.value) {
+        rtn += 'value="'+elm.value+'" ';
+    }
+    if(elm.embed) {
+        rtn += 'embed="'+elm.embed+"' ";
+    }
+    rtn += '/>';
+
+    return rtn;
 }
 
